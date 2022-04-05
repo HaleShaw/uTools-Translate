a4c5eecdb96e41ea799a253941df10be5c017afb
HaleShaw@163.com <HaleShaw@163.com>
2022-04-05 10:18:59 +0800

    remove the setting ui of lang

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -2,90 +2,48 @@ const options = {
   youDaoOld: {
     name: "内置有道",
     api: "http://fanyi.youdao.com/openapi.do?keyfrom=WoxLauncher&key=1247918016&type=data&doctype=json&version=1.1&q=",
-    langs: ["自动", "英语", "中文"],
   },
   youDaoWap: {
     name: "有道移动版",
     api: "https://m.youdao.com/dict?le=eng&q=",
-    langs: ["自动", "英语", "中文"],
   },
   youDaoWeb: {
     name: "有道网页版",
     api: "https://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule",
-    langs: ["自动", "英语", "中文"],
   },
   google: {
     name: "谷歌",
     api: "translate.google.cn",
     path: "/_/TranslateWebserverUi/data/batchexecute",
-    langs: ["自动", "英语", "中文"],
   },
   xiaoNiu: {
     name: "小牛",
     api: "https://test.niutrans.com/NiuTransServer/testaligntrans",
-    langs: ["自动", "英语", "中文"],
   },
   deepL: {
     name: "DeepL",
     api: "https://www2.deepl.com/jsonrpc?method=LMT_handle_jobs",
-    langs: ["自动", "英语", "中文"],
   },
   youDao: {
     name: "有道",
     api: "http://openapi.youdao.com/api",
-    langs: ["自动", "英语", "中文"],
   },
   baiDu: {
     name: "百度",
     api: "https://fanyi-api.baidu.com/api/trans/vip/translate",
-    langs: [
-      "自动",
-      "英语",
-      "中文",
-      "粤语",
-      "文言文",
-      "日语",
-      "韩语",
-      "法语",
-      "西班牙语",
-      "泰语",
-      "阿拉伯语",
-      "俄语",
-      "葡萄牙语",
-      "德语",
-      "意大利语",
-      "希腊语",
-      "荷兰语",
-      "波兰语",
-      "保加利亚语",
-      "爱沙尼亚语",
-      "丹麦语",
-      "芬兰语",
-      "捷克语",
-      "罗马尼亚语",
-      "斯洛文尼亚语",
-      "瑞典语",
-      "匈牙利语",
-      "繁体中文",
-      "越南语",
-    ],
   },
   aliYun: {
     name: "阿里",
     api: "mt.cn-hangzhou.aliyuncs.com",
     path: "/api/translate/web/general",
-    langs: ["自动", "英语", "中文"],
   },
   tencent: {
     name: "腾讯",
     api: "tmt.tencentcloudapi.com",
-    langs: ["自动", "英语", "中文", "日语", "韩语"],
   },
   caiYun: {
     name: "彩云小译",
     api: "http://api.interpreter.caiyunai.com/v1/translator",
-    langs: ["自动", "英语", "中文", "日语"],
-    langMap: { 自动: "auto", 英语: "en", 中文: "zh", 日语: "ja" },
   },
 };
 
@@ -100,14 +58,11 @@ const settingHeight = 544;
 
 const errMsgEmptyApp = "应用ID或密钥不能为空！";
 
-var settingObj = {};
-
 function initSetting() {
   utools.setExpendHeight(settingHeight);
   loadConfiguration();
   addSpeakListener();
   addLangListener();
-  addExchangeListener();
   addSiteListener();
   addSettingBtnListener();
   addEyeListener();
@@ -129,7 +84,6 @@ function loadConfiguration() {
   }
 
   loadSpeak();
-  loadLang();
 
   let youDaoAppId = utools.dbStorage.getItem("youDaoAppId");
   if (youDaoAppId) {
@@ -194,56 +148,6 @@ function loadSpeak() {
   } else {
     speakBtn.setAttribute("src", "./assets/images/speakOff.png");
   }
-}
-
-/**
- * Load the configuration of lang.
- */
-function loadLang() {
-  settingObj = utools.dbStorage.getItem("setting");
-
-  let services = Object.keys(options);
-  for (let j = 0; j < services.length; j++) {
-    let service = services[j];
-    if (!settingObj[service]) {
-      settingObj[service] = {};
-    }
-    if (!settingObj[service]["lang"]) {
-      settingObj[service]["lang"] = {};
-    }
-    let langs = options[service]["langs"];
-
-    let sourceValue = settingObj[service]["lang"]["source"] || langs[0];
-    let targetValue = settingObj[service]["lang"]["target"] || langs[0];
-
-    let sourceElement = document.querySelector(
-      `.service.${service}>.detail>.exchange>select.source`
-    );
-    let targetElement = document.querySelector(
-      `.service.${service}>.detail>.exchange>select.target`
-    );
-    let htmlStr = "";
-    for (let i = 0; i < langs.length; i++) {
-      htmlStr += `<option value=${langs[i]}>${langs[i]}</option>`;
-    }
-    sourceElement.innerHTML = htmlStr;
-    targetElement.innerHTML = htmlStr;
-    for (let i = 0; i < sourceElement.length; i++) {
-      if (sourceValue == sourceElement.options[i].value) {
-        sourceElement.options[i].selected = true;
-        break;
-      }
-    }
-    for (let i = 0; i < targetElement.length; i++) {
-      if (targetValue == targetElement.options[i].value) {
-        targetElement.options[i].selected = true;
-        break;
-      }
-    }
-  }
-
-  // Save the  setting object to prevent it change.
-  utools.dbStorage.setItem("setting", settingObj);
 }
 
 function saveConfiguration() {
@@ -311,51 +215,8 @@ function saveConfiguration() {
   utools.dbStorage.setItem("tencentAppId", tencentAppId);
   utools.dbStorage.setItem("tencentAppSecret", tencentAppSecret);
   utools.dbStorage.setItem("caiYunToken", caiYunToken);
-  saveLang();
-  utools.dbStorage.setItem("setting", settingObj);
   hideSetting();
-  utools.showNotification(
-    `当前API为：${options[option]["name"]}\n${settingObj[option]["lang"]["source"]} → ${settingObj[option]["lang"]["target"]}`
-  );
-}
-
-/**
- * Save the setting of language.
- */
-function saveLang() {
-  let exchanges = document.querySelectorAll(".exchange");
-  for (let i = 0; i < exchanges.length; i++) {
-    const service = exchanges[i].parentElement.parentElement.classList[1];
-    let sourceEle = exchanges[i].querySelector("select.source");
-    let targetEle = exchanges[i].querySelector("select.target");
-    const sourceValue = getSelectValue(sourceEle);
-    const targetValue = getSelectValue(targetEle);
-    if (!settingObj[service]) {
-      settingObj[service] = {};
-    }
-    if (!settingObj[service]["lang"]) {
-      settingObj[service]["lang"] = {};
-    }
-    settingObj[service]["lang"]["source"] = sourceValue;
-    settingObj[service]["lang"]["target"] = targetValue;
-  }
-}
-
-/**
- * Get the selected value of select element.
- * @param {Object} select The select element.
- * @returns The selected value of select element.
- */
-function getSelectValue(select) {
-  let selectOptions = select.options;
-  let value = "";
-  for (let i = 0; i < selectOptions.length; i++) {
-    if (selectOptions[i].selected) {
-      value = selectOptions[i].value;
-      break;
-    }
-  }
-  return value;
+  utools.showNotification(`切换为：${options[option]["name"]}成功！`);
 }
 
 /**
@@ -373,52 +234,10 @@ function addSpeakListener() {
   });
 }
 
-/**
- * Add the listener of select of language.
- */
-function addLangListener() {
-  $("select").each((index, element) => {
-    $(element).on("change", () => {
-      let brother = getBrother($(element)[0]);
-      let brotherValue = $(brother[0]).val();
-      let thisValue = $(element).val();
-      if ("自动" != brotherValue && thisValue == brotherValue) {
-        brother[0].selectedIndex = 0;
-      }
-    });
-  });
-}
-
-/**
- * Get the brother select element.
- * @param {Object} element The select element.
- * @returns The brother select element.
- */
-function getBrother(element) {
-  let brotherClass = "source" === element.className ? "target" : "source";
-  return $(element).parent().children(`select.${brotherClass}`);
-}
-
-/**
- * Add the exchange button listener.
- */
-function addExchangeListener() {
-  $(".exchange>.icon").each((index, element) => {
-    $(element).on("click", () => {
-      let sourceEle = $(element).siblings("select.source");
-      let targetEle = $(element).siblings("select.target");
-      let temp = sourceEle[0].selectedIndex;
-      sourceEle[0].selectedIndex = targetEle[0].selectedIndex;
-      targetEle[0].selectedIndex = temp;
-    });
-  });
-}
-
 function addSiteListener() {
   $("a.site").each((index, element) => {
     $(element).on("click", () => {
       const url = $(element).attr("href");
-      console.log(url);
       utools.shellOpenExternal(url);
     });
   });
