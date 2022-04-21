165d5329bd868309dcc1fe59e47cdf1fb87d81e8
HaleShaw@163.com <HaleShaw@163.com>
2022-04-21 08:32:46 +0800

    Change the default API to Google

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -276,7 +276,7 @@ const options = {
 };
 
 // Set the default API.
-const defaultAPI = Object.keys(options)[4];
+const defaultAPI = Object.keys(options)[3];
 
 const defaultSpeak = true;
 
--- assets/js/xiaoNiu.js
+++ assets/js/xiaoNiu.js
@@ -8,24 +8,38 @@ async function lookupXiaoNiu(word) {
   const url = api + `?from=${source}&to=${target}&src_text=${word}`;
 
   let response = await get(url);
-  let resData = JSON.parse(response);
-  const tran = resData?.tgt_text;
-  if (tran) {
-    let phoneticHtml = "";
-    if (speak) {
-      const phoneticEn = getPhoneticEn(word);
-      const phoneticUs = getPhoneticUs(word);
-      phoneticHtml = `<span>英</span>${phoneticEn}<span>美</span>${phoneticUs}`;
+  try {
+    let resData = JSON.parse(response);
+    const tran = resData?.tgt_text;
+    if (tran) {
+      let phoneticHtml = "";
+      if (speak) {
+        const phoneticEn = getPhoneticEn(word);
+        const phoneticUs = getPhoneticUs(word);
+        phoneticHtml = `<span>英</span>${phoneticEn}<span>美</span>${phoneticUs}`;
+      }
+      let dataTitle = `<span class="translation">${tran}</span>${phoneticHtml}`;
+      data.push({
+        title: dataTitle,
+        description: "基本释义",
+      });
+    } else {
+      data.push({
+        title: errTitle,
+        description: "翻译错误，可进入设置页面切换其他API",
+      });
     }
-    let dataTitle = `<span class="translation">${tran}</span>${phoneticHtml}`;
-    data.push({
-      title: dataTitle,
-      description: "基本释义",
-    });
-  } else {
+  } catch (error) {
+    let errorCode = response?.error_code;
+    if ("15001" == errorCode) {
+    }
+    let errorMsg =
+      "15001" == errorCode
+        ? "服务器异常，可进入设置页面切换其他API"
+        : "翻译错误，可进入设置页面切换其他API";
     data.push({
       title: errTitle,
-      description: "翻译错误，可进入设置页面切换其他API",
+      description: errorMsg,
     });
   }
   return data;
